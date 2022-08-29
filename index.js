0e3126613f90858cd798514d1473602240c8273b
+ <+>
2022-08-28 23:19:48 -0400

    commit tree bf38adc754f806b0229223cc1befc6bb99f6981a

---
+++ index.js
@@ -0,0 +1,50 @@
+function add() {
+}
+function subtract() {
+}
+function multiply() {
+}
+function divide() {
+}
+1 + 80; 
+60 - 40;
+2 * 3.4;
+5.0 / 2.5;
+
+function add() {
+    return "something";
+}
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+function increment(a) {
+    return ++a;
+}
+
+function decrement(a) {
+    return --a;
+}
+
+function makeInt(a) {
+    parseInt(a, 10);
+    return a;
+}
+
+function preserveDecimal(a) {
+    parseFloat(a);
+    return a;
+}
